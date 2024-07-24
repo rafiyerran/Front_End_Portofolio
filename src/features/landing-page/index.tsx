efac08067f7c200215c3e33af8c847b5aba4ac78
Rafiy Erran <rafiyerran@gmail.com>
2024-07-25 00:27:55 +0700

    JIRA 002: Fix/update build

--- src/features/landing-page/index.tsx
+++ src/features/landing-page/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 import Avatar from "../../assets/4k background.jfif"
 import ProjectCard from '../../components/project-card';
